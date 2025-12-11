c9a185e92c5bb80459ff1b40d9a4ba837dc65a33
muroyyy <amirul.faiz01@gmail.com>
2025-12-11 11:39:34 +0800

    feat: add HTTP/HTTPS access to Wazuh security group
    - Add ingress rule for port 80 (HTTP)
    - Add ingress rule for port 443 (HTTPS)
    - Allow web traffic from anywhere for Wazuh services

--- terraform/modules/ec2/wazuh_main.tf
+++ terraform/modules/ec2/wazuh_main.tf
@@ -19,6 +19,22 @@ resource "aws_security_group" "wazuh" {
     description = "Wazuh dashboard access"
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP access"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS access"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
